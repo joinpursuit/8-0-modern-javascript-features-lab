09dc7da9a5bb9fc035b66c16840c9fb2b3c4cab1
Christina Loiacono <loiaconochristina@gmail.com>
2021-08-30 14:04:29 -0400

    completed first function

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const moons = [
 
   You should not need to make any other changes to the code.
 */
-function organizeMoonsByPlanet(moons) {
+const organizeMoonsByPlanet = (moons) => {
   const result = {};
   for (let moon of moons) {
-    const name = moon.name;
-    const planet = moon.planet;
+    const { name } = moon;
+    const { planet } = moon;
     if (result[planet]) {
       result[planet].push(name);
     } else {
