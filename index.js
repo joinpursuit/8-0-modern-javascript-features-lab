dc871f3b7b0b74b489d4c6f44200e4531775ae74
“Lili <lilyhuang114classb@gmail.com>
2021-08-30 18:02:36 -0400

    Finished 8-0-modern-javascript-features-lab 08302021.

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ const moons = [
 
   You should not need to make any other changes to the code.
 */
-function organizeMoonsByPlanet(moons) {
+const organizeMoonsByPlanet = moons =>  {
   const result = {};
   for (let moon of moons) {
-    const name = moon.name;
-    const planet = moon.planet;
+    const {name, planet} = moon;
     if (result[planet]) {
       result[planet].push(name);
     } else {
@@ -27,6 +26,20 @@ function organizeMoonsByPlanet(moons) {
   }
   return result;
 }
+// function organizeMoonsByPlanet(moons) {
+
+//   const result = {};
+//   for (let moon of moons) {
+//     const name = moon.name;
+//     const planet = moon.planet;
+//     if (result[planet]) {
+//       result[planet].push(name);
+//     } else {
+//       result[planet] = [name];
+//     }
+//   }
+//   return result;
+// }
 
 /*
   To update this function, you should:
@@ -35,10 +48,8 @@ function organizeMoonsByPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function getClosestToPlanet(moons) {
-  let closest = moons[0];
-  const rest = moons.slice(1);
-
+const getClosestToPlanet = moons => {
+  let [closest, ...rest] = moons
   for (let moon of rest) {
     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
       closest = moon;
@@ -47,6 +58,18 @@ function getClosestToPlanet(moons) {
 
   return closest.name;
 }
+// function getClosestToPlanet(moons) {
+//   let closest = moons[0];
+//   const rest = moons.slice(1);
+
+//   for (let moon of rest) {
+//     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
+//       closest = moon;
+//     }
+//   }
+
+//   return closest.name;
+// }
 
 /*
   To update this function, you should:
@@ -56,19 +79,29 @@ function getClosestToPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function createMoon(name, planet, distanceFromPlanetInKm) {
+const createMoon = (name, planet, distanceFromPlanetInKm = "Unknown") => {
   if (!name || !planet) {
     return "Name and planet are required.";
   }
-
-  distanceFromPlanetInKm = distanceFromPlanetInKm || "Unknown";
-
   return {
-    name: name,
-    planet: planet,
-    distanceFromPlanetInKm: distanceFromPlanetInKm,
+    name,
+    planet,
+    distanceFromPlanetInKm,
   };
 }
+// function createMoon(name, planet, distanceFromPlanetInKm) {
+//   if (!name || !planet) {
+//     return "Name and planet are required.";
+//   }
+
+//   distanceFromPlanetInKm = distanceFromPlanetInKm || "Unknown";
+
+//   return {
+//     name: name,
+//     planet: planet,
+//     distanceFromPlanetInKm: distanceFromPlanetInKm,
+//   };
+// }
 
 module.exports = {
   organizeMoonsByPlanet,
