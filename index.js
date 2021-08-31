a195a7cd0bf4fb3c38217abcf4760ef4dcaa2dde
stephanie lucano <stephaniemarielucano@gmail.com>
2021-08-30 23:17:34 -0400

    complete

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const moons = [
 
   You should not need to make any other changes to the code.
 */
-function organizeMoonsByPlanet(moons) {
+const organizeMoonsByPlanet = (moons) => {
   const result = {};
   for (let moon of moons) {
-    const name = moon.name;
-    const planet = moon.planet;
+    const {name} = moon;
+    const {planet} = moon;
     if (result[planet]) {
       result[planet].push(name);
     } else {
@@ -27,6 +27,7 @@ function organizeMoonsByPlanet(moons) {
   }
   return result;
 }
+console.log(organizeMoonsByPlanet(moons)); //> {Earth: ['Luna'], Mars: ['Deimos', 'Phobos'], Jupiter: ['Ganymede', 'Io', 'Europa']}
 
 /*
   To update this function, you should:
@@ -35,18 +36,20 @@ function organizeMoonsByPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function getClosestToPlanet(moons) {
-  let closest = moons[0];
-  const rest = moons.slice(1);
+const getClosestToPlanet = (moons) => {
+  let {...closest} = moons[0];
+  // console.log(closest)
+  const [...rest] = moons.slice(1);
+  // console.log(rest)
 
   for (let moon of rest) {
     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
       closest = moon;
     }
   }
-
   return closest.name;
 }
+console.log(getClosestToPlanet(moons)) //> 'Phobos'
 
 /*
   To update this function, you should:
@@ -56,7 +59,7 @@ function getClosestToPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function createMoon(name, planet, distanceFromPlanetInKm) {
+const createMoon = (name, planet, distanceFromPlanetInKm = "Unknown") => {
   if (!name || !planet) {
     return "Name and planet are required.";
   }
@@ -64,11 +67,12 @@ function createMoon(name, planet, distanceFromPlanetInKm) {
   distanceFromPlanetInKm = distanceFromPlanetInKm || "Unknown";
 
   return {
-    name: name,
-    planet: planet,
-    distanceFromPlanetInKm: distanceFromPlanetInKm,
+    name,
+    planet,
+    distanceFromPlanetInKm,
   };
 }
+console.log(createMoon("Europa", "Jupiter", 670900)) //> {name: 'Europa', planet: 'Jupiter', distanceFromPlanetInKm: 670900}
 
 module.exports = {
   organizeMoonsByPlanet,
