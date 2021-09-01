4836cf506307a79fe0bc09e8ebef053757ececa4
Samantha Gosda <gosdasam@gmail.com>
2021-09-01 14:24:07 -0400

    completed the getClosestToPlanet func

--- index.js
+++ index.js
@@ -34,9 +34,8 @@ const organizeMoonsByPlanet = (moons) => {
 
   You should not need to make any other changes to the code.
 */
-function getClosestToPlanet(moons) {
-  let closest = moons[0];
-  const rest = moons.slice(1);
+const getClosestToPlanet = (moons) => {
+  let [closest, ...rest] = moons;
 
   for (let moon of rest) {
     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
