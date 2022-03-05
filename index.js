4341ac242275e8d8c08f95c41a1c149e561f6115
Denisse Maldonado <denissemaldonado@Denisses-MacBook-Air.local>
2022-03-05 15:09:54 -0500

    finished lab

--- index.js
+++ index.js
@@ -14,11 +14,25 @@ const moons = [
 
   You should not need to make any other changes to the code.
 */
-function organizeMoonsByPlanet(moons) {
-  const result = {};
+// function organizeMoonsByPlanet(moons) {
+//   const result = {};
+//   for (let moon of moons) {
+//     const name = moon.name;
+//     const planet = moon.planet;
+//     if (result[planet]) {
+//       result[planet].push(name);
+//     } else {
+//       result[planet] = [name];
+//     }
+//   }
+//   //return result;
+//   console.log(result)
+// }
+
+const organizeMoonsByPlanet = moons => {
+  let result = {};
   for (let moon of moons) {
-    const name = moon.name;
-    const planet = moon.planet;
+    const { name, planet } = moon
     if (result[planet]) {
       result[planet].push(name);
     } else {
@@ -28,6 +42,7 @@ function organizeMoonsByPlanet(moons) {
   return result;
 }
 
+
 /*
   To update this function, you should:
   - Change the function to use arrow syntax, while keeping the name of the function the same.
@@ -35,16 +50,27 @@ function organizeMoonsByPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function getClosestToPlanet(moons) {
-  let closest = moons[0];
-  const rest = moons.slice(1);
+// function getClosestToPlanet(moons) {
+//   let closest = moons[0];
+//   const rest = moons.slice(1);
 
+//   for (let moon of rest) {
+//     console.log(moon);
+//     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
+//       closest = moon;
+//     }
+//   }
+//   console.log(closest.name);
+//   // return closest.name;
+// }
+
+const getClosestToPlanet = moons => {
+  let [closest, ...rest] = moons;
   for (let moon of rest) {
     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
       closest = moon;
     }
   }
-
   return closest.name;
 }
 
@@ -56,18 +82,28 @@ function getClosestToPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function createMoon(name, planet, distanceFromPlanetInKm) {
+// function createMoon(name, planet, distanceFromPlanetInKm) {
+//   if (!name || !planet) {
+//     return "Name and planet are required.";
+//   }
+
+//   distanceFromPlanetInKm = distanceFromPlanetInKm || "Unknown";
+
+//   return {
+//     name: name,
+//     planet: planet,
+//     distanceFromPlanetInKm: distanceFromPlanetInKm,
+//   };
+// }
+
+const createMoon = (name, planet, distanceFromPlanetInKm = "Unknown") => {
   if (!name || !planet) {
     return "Name and planet are required.";
   }
 
-  distanceFromPlanetInKm = distanceFromPlanetInKm || "Unknown";
+  distanceFromPlanetInKm = distanceFromPlanetInKm;
 
-  return {
-    name: name,
-    planet: planet,
-    distanceFromPlanetInKm: distanceFromPlanetInKm,
-  };
+  return {name, planet, distanceFromPlanetInKm}
 }
 
 module.exports = {
