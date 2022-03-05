72b8744fed2cded69f77ea0a152b79ca6238e7e1
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2022-03-05 16:19:53 -0500

    Javascript Lab

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ const moons = [
 
   You should not need to make any other changes to the code.
 */
-function organizeMoonsByPlanet(moons) {
+ organizeMoonsByPlanet = (moons) => {
   const result = {};
   for (let moon of moons) {
-    const name = moon.name;
-    const planet = moon.planet;
+    const {name , planet} = moon
     if (result[planet]) {
       result[planet].push(name);
     } else {
@@ -35,9 +34,11 @@ function organizeMoonsByPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function getClosestToPlanet(moons) {
-  let closest = moons[0];
-  const rest = moons.slice(1);
+ getClosestToPlanet = (moons) => {
+  let [closest, ...rest] = moons;
+  // console.log(closest)
+  // const rest = moons.slice(1);
+  // console.log(rest)
 
   for (let moon of rest) {
     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
@@ -56,17 +57,14 @@ function getClosestToPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function createMoon(name, planet, distanceFromPlanetInKm) {
+ createMoon = (name, planet, distanceFromPlanetInKm = "Unknown") => {
   if (!name || !planet) {
     return "Name and planet are required.";
   }
-
-  distanceFromPlanetInKm = distanceFromPlanetInKm || "Unknown";
-
   return {
-    name: name,
-    planet: planet,
-    distanceFromPlanetInKm: distanceFromPlanetInKm,
+     name,
+   planet,
+   distanceFromPlanetInKm,
   };
 }
 
