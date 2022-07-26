3198ce6e333d9d4ff6c2a9a5a6f9ac8185b86f59
MarsiaRochelin <marsrochelin@pursuit.org>
2022-07-26 18:41:46 -0400

    complete

--- index.js
+++ index.js
@@ -53,17 +53,15 @@ const getClosestToPlanet = (moons) => {
 
   You should not need to make any other changes to the code.
 */
-function createMoon (name, planet, distanceFromPlanetInKm) {
+const createMoon = (name, planet, distanceFromPlanetInKm = "Unknown") => {
   if (!name || !planet) {
     return "Name and planet are required.";
   }
 
-  distanceFromPlanetInKm = distanceFromPlanetInKm || "Unknown";
-
   return {
-    name: name,
-    planet: planet,
-    distanceFromPlanetInKm: distanceFromPlanetInKm,
+    name,
+    planet,
+    distanceFromPlanetInKm,
   };
 }
 
