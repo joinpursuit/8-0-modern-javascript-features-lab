277fc818ab844625747686ed002da0817618d8a3
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-07-26 17:48:41 -0400

    tests passing

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const moons = [
 
   You should not need to make any other changes to the code.
 */
-const organizeMoonsByPlanet = moons => {
+const organizeMoonsByPlanet = (moons) => {
   const result = {};
   for (let moon of moons) {
-    const {name, planet} = moon;
+    const { name, planet } = moon;
     if (result[planet]) {
       result[planet].push(name);
     } else {
@@ -25,7 +25,7 @@ const organizeMoonsByPlanet = moons => {
     }
   }
   return result;
-}
+};
 
 /*
   To update this function, you should:
@@ -34,18 +34,15 @@ const organizeMoonsByPlanet = moons => {
 
   You should not need to make any other changes to the code.
 */
-function getClosestToPlanet(moons) {
-  let closest = moons[0];
-  const rest = moons.slice(1);
-
+const getClosestToPlanet = (moons) => {
+  let [closest, ...rest] = moons;
   for (let moon of rest) {
     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
       closest = moon;
     }
   }
-
   return closest.name;
-}
+};
 
 /*
   To update this function, you should:
@@ -55,19 +52,16 @@ function getClosestToPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function createMoon(name, planet, distanceFromPlanetInKm) {
+const createMoon = (name, planet, distanceFromPlanetInKm = "Unknown") => {
   if (!name || !planet) {
     return "Name and planet are required.";
   }
-
-  distanceFromPlanetInKm = distanceFromPlanetInKm || "Unknown";
-
   return {
-    name: name,
-    planet: planet,
-    distanceFromPlanetInKm: distanceFromPlanetInKm,
+    name,
+    planet,
+    distanceFromPlanetInKm,
   };
-}
+};
 
 module.exports = {
   organizeMoonsByPlanet,
