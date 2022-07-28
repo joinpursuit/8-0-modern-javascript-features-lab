e29875a4d7c74f09a3105b0b500d7554735fcd67
youssefmohamedfarouk <youssefmohamedfarouk@pursuit.org>
2022-07-28 10:58:04 -0400

    Add solutions for organizeMoonsByPlanet(), getClosestToPlanet(), and createMoon() functions

--- index.js
+++ index.js
@@ -14,16 +14,11 @@ const moons = [
 
   You should not need to make any other changes to the code.
 */
-function organizeMoonsByPlanet(moons) {
+const organizeMoonsByPlanet = (moons) => {
   const result = {};
   for (let moon of moons) {
-    const name = moon.name;
-    const planet = moon.planet;
-    if (result[planet]) {
-      result[planet].push(name);
-    } else {
-      result[planet] = [name];
-    }
+    const {name, planet} = moon;
+    result[planet] ? result[planet].push(name) : result[planet] = [name];
   }
   return result;
 }
@@ -35,9 +30,8 @@ function organizeMoonsByPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function getClosestToPlanet(moons) {
-  let closest = moons[0];
-  const rest = moons.slice(1);
+const getClosestToPlanet = (moons) => {
+  let [closest, ...rest] = moons;
 
   for (let moon of rest) {
     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
@@ -56,18 +50,12 @@ function getClosestToPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function createMoon(name, planet, distanceFromPlanetInKm) {
+const createMoon = (name, planet, distanceFromPlanetInKm = "Unknown") => {
   if (!name || !planet) {
     return "Name and planet are required.";
   }
-
-  distanceFromPlanetInKm = distanceFromPlanetInKm || "Unknown";
-
-  return {
-    name: name,
-    planet: planet,
-    distanceFromPlanetInKm: distanceFromPlanetInKm,
-  };
+  
+  return {name, planet, distanceFromPlanetInKm};
 }
 
 module.exports = {
