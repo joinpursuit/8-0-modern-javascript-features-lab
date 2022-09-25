9a1aaf03cfad5cae13153a57746b91e035841416
cjs2408 <carlossuero@pursuit.org>
2022-09-24 21:23:44 -0500

    i should pass all test but its not

--- index.js
+++ index.js
@@ -11,30 +11,12 @@ const moons = [
   To update this function, you should:
   - Change the function to use arrow syntax, while keeping the name of the function the same.
   - Use object destructuring to assign the `name` and `planet` variables.
-
   You should not need to make any other changes to the code.
 */
-
-function organizeMoonsByPlanet(moons) {
-  const organizeMoonsByPlanet = moons => {
-    const result = {};
-    for (let moon of moons) {
-      const name = moon.name;
-      const planet = moon.planet;
-      const {name, planet} = moon;
-      if (result[planet]) {
-        result[planet].push(name);
-      } else {
-      }
-      }
-      return result;
-      }
-    }
-  
+const organizeMoonsByPlanet = (moons) => {
   const result = {};
   for (let moon of moons) {
-    const name = moon.name;
-    const planet = moon.planet;
+    const {name, planet} = moon;
     if (result[planet]) {
       result[planet].push(name);
     } else {
@@ -44,41 +26,38 @@ function organizeMoonsByPlanet(moons) {
   return result;
 }
 
-
 /*
   To update this function, you should:
   - Change the function to use arrow syntax, while keeping the name of the function the same.
   - Use object destructuring and the rest operator to assign the `closest` and `rest` variables.
-
   You should not need to make any other changes to the code.
 */
-function getClosestToPlanet(moons) {
-  let closest = moons[0];
-  const rest = moons.slice(1);
 const getClosestToPlanet = (moons) => {
-
   let [closest, ...rest] = moons;
 
   for (let moon of rest) {
     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
+      closest = moon;
     }
   }
+
+  return closest.name;
 }
-}
+
 /*
   To update this function, you should:
   - Change the function to use arrow syntax, while keeping the name of the function the same.
   - Set the default value of `distanceFromPlanetInKm` to "Unknown" as part of the function signature.
   - Use object shorthand to create the new object.
-
   You should not need to make any other changes to the code.
 */
-function createMoon(name, planet, distanceFromPlanetInKm) {
-  const createMoon = (name, planet, distanceFromPlanetInKm = "Unknown") => {
-    if (!name || !planet) {
-      return "Name and planet are required.";
-    }
-  
+const createMoon = (name, planet, distanceFromPlanetInKm = "Unknown") => {
+  if (!name || !planet) {
+    return "Name and planet are required.";
+  }
+
+  return { name, planet, distanceFromPlanetInKm };
+}
 
 module.exports = {
   organizeMoonsByPlanet,
