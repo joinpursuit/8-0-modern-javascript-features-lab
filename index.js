1a1f1c6207336fb5d049815c4506a5d09451296e
mrkwab3 <mrkwab3@gmail.com>
2022-11-15 15:38:27 -0500

    Removed filler

--- index.js
+++ index.js
@@ -62,8 +62,6 @@ const createMoon = (name, planet, distanceFromPlanetInKm = "Unknown") => {
     return "Name and planet are required.";
   } 
 
-  distanceFromPlanetInKm = distanceFromPlanetInKm || "Unknown";
-
   return {
     name,
     planet,
