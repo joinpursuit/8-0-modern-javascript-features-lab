ca4d93dda5374fb07fb564a744c41bc26c1bd3b7
jkpasaoa <jacqueline.pasaoa@gmail.com>
2022-11-15 21:30:57 -0500

    second function completed. done

--- index.js
+++ index.js
@@ -53,9 +53,10 @@ const moons = [
 
   You should not need to make any other changes to the code.
 */
-function getClosestToPlanet(moons) {
-  let closest = moons[0];
-  const rest = moons.slice(1);
+const getClosestToPlanet= (moons) => {
+  let[ closest, ...rest ] = moons;
+  //let closest = moons[0];
+  //const rest = moons.slice(1);
 
   for (let moon of rest) {
     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
