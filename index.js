9cc1279f75b684820f1fc5bf79d199573ba463b8
Fig <bethanytrr@gmail.com>
2022-11-16 10:28:33 -0500

    Lab completed, delayed due to technical issues.

--- index.js
+++ index.js
@@ -14,18 +14,17 @@ const moons = [
 
   You should not need to make any other changes to the code.
 */
-function organizeMoonsByPlanet(moons) {
-  const result = {};
-  for (let moon of moons) {
-    const name = moon.name;
-    const planet = moon.planet;
+const organizeMoonsByPlanet = (moons) => {
+	const result = {};
+	for (let moon of moons) {
+    const {name, planet} = moon;
     if (result[planet]) {
       result[planet].push(name);
     } else {
       result[planet] = [name];
-    }
-  }
-  return result;
+		}
+	}
+	return result;
 }
 
 /*
@@ -35,9 +34,8 @@ function organizeMoonsByPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function getClosestToPlanet(moons) {
-  let closest = moons[0];
-  const rest = moons.slice(1);
+const getClosestToPlanet = (moons) => {
+  let [closest, ...rest] = moons;
 
   for (let moon of rest) {
     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
@@ -56,7 +54,7 @@ function getClosestToPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function createMoon(name, planet, distanceFromPlanetInKm) {
+const createMoon = (name, planet, distanceFromPlanetInKm = "Unknown") => {
   if (!name || !planet) {
     return "Name and planet are required.";
   }
@@ -64,9 +62,7 @@ function createMoon(name, planet, distanceFromPlanetInKm) {
   distanceFromPlanetInKm = distanceFromPlanetInKm || "Unknown";
 
   return {
-    name: name,
-    planet: planet,
-    distanceFromPlanetInKm: distanceFromPlanetInKm,
+    name, planet, distanceFromPlanetInKm,
   };
 }
 
